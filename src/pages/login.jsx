87dfb5d555e01b4e3028bdedd5f60064a1251c91
Viviane <vivianeaguiarc@outlook.com>
2025-12-21 11:43:56 -0300

    feat: add login with form validations

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,105 @@
-const LoginPage = () => {
-  return <h1>Login Page</h1>
+ 
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { z } from 'zod'
+
+import PasswordInput from '@/components/password-input'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+const loginSchema = z.object({
+  email: z.string().trim().email({
+    message: 'Insira um email válido.',
+  }),
+  password: z.string().min(6, {
+    message: 'A senha deve ter no mínimo 6 caracteres.',
+  }),
+})
+
+const Login = () => {
+  const methods = useForm({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  const handleSubmit = (data) => {
+    console.log(data)
+  }
+
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-3">
+      <Form {...methods}>
+        <form onSubmit={methods.handleSubmit(handleSubmit)}>
+          <Card className="w-[500px]">
+            <CardHeader>
+              <CardTitle>Entrar</CardTitle>
+              <CardDescription>
+                Acesse sua conta informando seus dados.
+              </CardDescription>
+            </CardHeader>
+
+            <CardContent className="space-y-4">
+              <FormField
+                control={methods.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={methods.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Senha</FormLabel>
+                    <FormControl>
+                      <PasswordInput {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+
+            <CardFooter className="flex flex-col gap-3">
+              <Button className="w-full">Entrar</Button>
+
+              <Button variant="link" asChild>
+                <Link to="/signup">Ainda não tem conta? Crie uma</Link>
+              </Button>
+            </CardFooter>
+          </Card>
+        </form>
+      </Form>
+    </div>
+  )
 }
 
-export default LoginPage
+export default Login
