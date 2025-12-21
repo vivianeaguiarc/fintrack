72a2478c396954e6789af84b31f10151f2eb2879
Viviane <vivianeaguiarc@outlook.com>
2025-12-21 09:15:02 -0300

    feat: add password input component

---
+++ src/components/password-input.jsx
@@ -0,0 +1,26 @@
+import { EyeIcon, EyeOffIcon } from 'lucide-react'
+import { useState } from 'react'
+
+import { Button } from './ui/button'
+import { Input } from './ui/input'
+
+const PasswordInput = ({ placeholder = 'Digite sua senha' }) => {
+  const [passwordIsVisible, setPasswordIsVisible] = useState(false)
+  return (
+    <div className="relative">
+      <Input
+        type={passwordIsVisible ? 'text' : 'password'}
+        placeholder={placeholder}
+      />
+      <Button
+        variant="ghost"
+        className="absolute bottom-0 right-0 top-0 my-auto mr-1 h-8 w-8 text-muted-foreground"
+        onClick={() => setPasswordIsVisible((prev) => !prev)}
+      >
+        {passwordIsVisible ? <EyeOffIcon /> : <EyeIcon />}
+      </Button>
+    </div>
+  )
+}
+
+export default PasswordInput
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,7 +1,6 @@
-import { EyeIcon, EyeOffIcon } from 'lucide-react'
-import { useState } from 'react'
 import { Link } from 'react-router'
 
+import PasswordInput from '@/components/password-input'
 import { Button } from '@/components/ui/button'
 import {
   Card,
@@ -14,7 +13,6 @@ import {
 import { Input } from '@/components/ui/input'
 
 const Signup = () => {
-  const [passwordIsVisible, setPasswordIsVisible] = useState(false)
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-3">
       <Card className="w-[500px]">
@@ -28,19 +26,8 @@ const Signup = () => {
           <Input placeholder="Digite seu nome" />
           <Input placeholder="Digite seu sobrenome" />
           <Input placeholder="Digite seu email" />
-          <div className="relative">
-            <Input
-              type={passwordIsVisible ? 'text' : 'password'}
-              placeholder="Digite sua senha"
-            />
-            <Button
-              variant="ghost"
-              className="absolute bottom-0 right-0 top-0 my-auto mr-1 h-8 w-8 text-muted-foreground"
-              onClick={() => setPasswordIsVisible((prev) => !prev)}
-            >
-              {passwordIsVisible ? <EyeOffIcon /> : <EyeIcon />}
-            </Button>
-          </div>
+          <PasswordInput />
+          <PasswordInput placeholder="Digite sua senha novamente" />
         </CardContent>
         <CardFooter>
           <Button className="w-full">Criar Conta</Button>
