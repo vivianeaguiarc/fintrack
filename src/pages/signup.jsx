140987eebf69c90f343ec46def140c8ff3800149
Viviane <vivianeaguiarc@outlook.com>
2025-12-22 15:22:07 -0300

    feat: ensure password and password confirmation are the same on signup form

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -29,27 +29,36 @@ import {
 import { Input } from '@/components/ui/input'
 import { api } from '@/lib/axios'
 
-const signupSchema = z.object({
-  firstName: z.string().trim().min(2, {
-    message: 'O nome deve ter no mínimo 2 caracteres.',
-  }),
-  lastName: z.string().trim().min(2, {
-    message: 'O sobrenome deve ter no mínimo 2 caracteres.',
-  }),
-  email: z.string().trim().email({
-    message: 'Insira um email válido.',
-  }),
-  password: z.string().min(6, {
-    message: 'A senha deve ter no mínimo 6 caracteres.',
-  }),
-  confirmPassword: z.string().min(6, {
-    message: 'A confirmação de senha deve ter no mínimo 6 caracteres.',
-  }),
-  terms: z.boolean().refine((value) => value === true, {
-    message: 'Você deve aceitar os termos de uso e política de privacidade.',
-  }),
-})
-
+const signupSchema = z
+  .object({
+    firstName: z.string().trim().min(2, {
+      message: 'O nome deve ter no mínimo 2 caracteres.',
+    }),
+    lastName: z.string().trim().min(2, {
+      message: 'O sobrenome deve ter no mínimo 2 caracteres.',
+    }),
+    email: z.string().trim().email({
+      message: 'Insira um email válido.',
+    }),
+    password: z.string().min(6, {
+      message: 'A senha deve ter no mínimo 6 caracteres.',
+    }),
+    confirmPassword: z.string().min(6, {
+      message: 'A confirmação de senha deve ter no mínimo 6 caracteres.',
+    }),
+    terms: z.boolean().refine((value) => value === true, {
+      message: 'Você deve aceitar os termos de uso e política de privacidade.',
+    }),
+  })
+  .refine(
+    (data) => {
+      return data.password === data.confirmPassword
+    },
+    {
+      message: 'As senhas não coincidem.',
+      path: ['confirmPassword'],
+    }
+  )
 const Signup = () => {
   const [user, setUser] = useState(null)
 
