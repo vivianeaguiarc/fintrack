1e4bf2d5384fdfc56a44086b646644ed1eeb4fd9
Viviane <vivianeaguiarc@outlook.com>
2025-12-23 13:02:31 -0300

    refactor: ensure user properties are returned from service on CamelCase

--- src/services/user.js
+++ src/services/user.js
@@ -8,17 +8,34 @@ export const UserService = {
       email: input.email,
       password: input.password,
     })
-    return response.data
+    return {
+      id: response.data.id,
+      firstName: response.data.first_name,
+      lastName: response.data.last_name,
+      email: response.data.email,
+      tokens: response.data.tokens,
+    }
   },
   login: async (input) => {
     const response = await publicApi.post('/users/login', {
       email: input.email,
       password: input.password,
     })
-    return response.data
+    return {
+      id: response.data.id,
+      firstName: response.data.first_name,
+      lastName: response.data.last_name,
+      email: response.data.email,
+      tokens: response.data.tokens,
+    }
   },
   me: async () => {
     const response = await protectedApi.get('/users/me')
-    return response.data
+    return {
+      id: response.data.id,
+      firstName: response.data.first_name,
+      lastName: response.data.last_name,
+      email: response.data.email,
+    }
   },
 }
