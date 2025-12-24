df64967a1ffa8a33918921de53a20360771b9ffe
Viviane <vivianeaguiarc@outlook.com>
2025-12-24 16:19:02 -0300

    feat: ensure selected date is persisted on page reload

--- src/components/date-selection.jsx
+++ src/components/date-selection.jsx
@@ -1,13 +1,37 @@
-import { addMonths } from 'date-fns'
-import { useState } from 'react'
+import { addMonths, format } from 'date-fns'
+import { useEffect, useState } from 'react'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 
 import { DatePickerWithRange } from './ui/date-picker-with-ranger'
 
+const formatDateToQueryParam = (date) => format(date, 'yyyy-MM-dd')
+
 const DateSelection = () => {
+  const [searchParams] = useSearchParams()
+  const navigate = useNavigate()
   const [date, setDate] = useState({
-    from: new Date(),
-    to: addMonths(new Date(), 1),
+    from: searchParams.get('from')
+      ? new Date(searchParams.get('from') + 'T00:00:00')
+      : new Date(),
+    to: searchParams.get('to')
+      ? new Date(searchParams.get('to') + 'T00:00:00')
+      : addMonths(new Date(), 1),
   })
+
+  // 1. sempre que o state "date" mudar, eu preciso persistir na url
+  useEffect(() => {
+    // early return
+    if (!date?.from || !date?.to) return
+
+    const queryParams = new URLSearchParams()
+    queryParams.set('from', formatDateToQueryParam(date.from))
+    queryParams.set('to', formatDateToQueryParam(date.to))
+
+    navigate(`/?${queryParams.toString()}`)
+  }, [navigate, date])
+
+  // 2. quando eu recarregar a página, eu pego o from e to da url
+
   return <DatePickerWithRange value={date} onChange={setDate} />
 }
 
