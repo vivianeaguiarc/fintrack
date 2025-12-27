48c90d300d877cf1f77e20239d6f73b60c0d299b
Viviane <vivianeaguiarc@outlook.com>
2025-12-27 16:17:16 -0300

    feat: add loading to signup button

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,181 +1,5 @@
-// /* eslint-disable react/no-unescaped-entities */
-// import { Loader2Icon } from 'lucide-react'
-// import { Link, Navigate } from 'react-router-dom'
-
-// import PasswordInput from '@/components/password-input'
-// import { Button } from '@/components/ui/button'
-// import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardFooter,
-//   CardHeader,
-//   CardTitle,
-// } from '@/components/ui/card'
-// import { Checkbox } from '@/components/ui/checkbox'
-// import {
-//   Form,
-//   FormControl,
-//   FormField,
-//   FormItem,
-//   FormLabel,
-//   FormMessage,
-// } from '@/components/ui/form'
-// import { Input } from '@/components/ui/input'
-// import { useAuthContext } from '@/context/auth'
-// import { useSignupForm } from '@/forms/hooks/user'
-
-// const Signup = () => {
-//   const { user, signup, isInitializing } = useAuthContext()
-
-//   const { form } = useSignupForm()
-
-//   const handleSubmit = (data) => signup(data)
-//   if (user) {
-//     return <Navigate to="/" />
-//   }
-//   if (isInitializing) {
-//     return (
-//       <div className="flex h-screen w-screen items-center justify-center">
-//         <Loader2Icon className="h-6 w-6 animate-spin" />
-//       </div>
-//     )
-//   }
-
-//   return (
-//     <div className="flex h-screen w-screen flex-col items-center justify-center gap-3">
-//       <Form {...form}>
-//         <form onSubmit={form.handleSubmit(handleSubmit)}>
-//           <Card className="w-[500px]">
-//             <CardHeader>
-//               <CardTitle>Crie a sua conta</CardTitle>
-//               <CardDescription>
-//                 Insira seus dados para criar uma nova conta.
-//               </CardDescription>
-//             </CardHeader>
-//             <CardContent className="space-y-4">
-//               <FormField
-//                 control={form.control}
-//                 name="firstName"
-//                 render={({ field }) => (
-//                   <FormItem>
-//                     <FormLabel>Nome</FormLabel>
-//                     <FormControl>
-//                       <Input placeholder="Digite seu nome" {...field} />
-//                     </FormControl>
-//                     <FormMessage />
-//                   </FormItem>
-//                 )}
-//               />
-//               <FormField
-//                 control={form.control}
-//                 name="lastName"
-//                 render={({ field }) => (
-//                   <FormItem>
-//                     <FormLabel>Sobrenome</FormLabel>
-//                     <FormControl>
-//                       <Input placeholder="Digite seu nome" {...field} />
-//                     </FormControl>
-//                     <FormMessage />
-//                   </FormItem>
-//                 )}
-//               />
-//               <FormField
-//                 control={form.control}
-//                 name="email"
-//                 render={({ field }) => (
-//                   <FormItem>
-//                     <FormLabel>Email</FormLabel>
-//                     <FormControl>
-//                       <Input placeholder="Digite seu email" {...field} />
-//                     </FormControl>
-//                     <FormMessage />
-//                   </FormItem>
-//                 )}
-//               />
-//               <FormField
-//                 control={form.control}
-//                 name="password"
-//                 render={({ field }) => (
-//                   <FormItem>
-//                     <FormLabel>Senha</FormLabel>
-//                     <FormControl>
-//                       <PasswordInput {...field} />
-//                     </FormControl>
-//                     <FormMessage />
-//                   </FormItem>
-//                 )}
-//               />
-//               <FormField
-//                 control={form.control}
-//                 name="confirmPassword"
-//                 render={({ field }) => (
-//                   <FormItem>
-//                     <FormLabel>Confirme a senha</FormLabel>
-//                     <FormControl>
-//                       <PasswordInput {...field} />
-//                     </FormControl>
-//                     <FormMessage />
-//                   </FormItem>
-//                 )}
-//               />
-//               <FormField
-//                 control={form.control}
-//                 name="terms"
-//                 render={({ field }) => (
-//                   <FormItem className="items-top flex space-x-2">
-//                     <FormControl>
-//                       <Checkbox
-//                         checked={field.value}
-//                         onCheckedChange={field.onChange}
-//                         id="terms"
-//                       />
-//                     </FormControl>
-//                     <div className="grid gap-1.5 leading-none">
-//                       <FormLabel
-//                         htmlFor="terms"
-//                         className={`opacity-70" text-xs text-muted-foreground ${
-//                           form.formState.errors.terms ? 'text-red-500' : ''
-//                         }`}
-//                       >
-//                         Ao clicar em "Criar Conta", você concorda com nossos{' '}
-//                         <a
-//                           href="#"
-//                           className={`text-white underline ${form.formState.errors.terms ? 'text-red-500' : ''}`}
-//                         >
-//                           termos de uso e política de privacidade.
-//                         </a>
-//                       </FormLabel>
-//                     </div>
-//                   </FormItem>
-//                 )}
-//               />
-//             </CardContent>
-//             <CardFooter>
-//               <Button className="w-full" disabled={form.formState.isSubmitting}>
-//                 {form.formState.isSubmitting && (
-//                   <Loader2Icon className="animate-spin" />
-//                 )}
-//                 {!form.formState.isSubmitting && 'Criar Conta'}
-//               </Button>
-//             </CardFooter>
-//           </Card>
-//         </form>
-//       </Form>
-//       <div className="flex items-center justify-center">
-//         <p className="text-center opacity-50">Já possui uma conta?</p>
-//         <Button asChild variant="link">
-//           <Link to="/login">Faça login</Link>
-//         </Button>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Signup
-
 import { Loader2Icon } from 'lucide-react'
-import { Link, Navigate } from 'react-router'
+import { Link, Navigate } from 'react-router-dom'
 
 import PasswordInput from '@/components/password-input'
 import { Button } from '@/components/ui/button'
@@ -222,7 +46,6 @@ const SignupPage = () => {
               <CardDescription>Insira os seus dados abaixo.</CardDescription>
             </CardHeader>
             <CardContent className="space-y-4">
-              {/* PRIMEIRO NOME */}
               <FormField
                 control={form.control}
                 name="firstName"
@@ -236,7 +59,6 @@ const SignupPage = () => {
                   </FormItem>
                 )}
               />
-              {/* ÚLTIMO NOME */}
               <FormField
                 control={form.control}
                 name="lastName"
@@ -250,7 +72,6 @@ const SignupPage = () => {
                   </FormItem>
                 )}
               />
-              {/* E-MAIL */}
               <FormField
                 control={form.control}
                 name="email"
@@ -264,7 +85,6 @@ const SignupPage = () => {
                   </FormItem>
                 )}
               />
-              {/* SENHA */}
               <FormField
                 control={form.control}
                 name="password"
@@ -278,11 +98,9 @@ const SignupPage = () => {
                   </FormItem>
                 )}
               />
-
-              {/* CONFIRMAÇÃO DE SENHA */}
               <FormField
                 control={form.control}
-                name="passwordConfirmation"
+                name="confirmPassword"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Confirmação de senha</FormLabel>
@@ -304,8 +122,11 @@ const SignupPage = () => {
                   <FormItem className="items-top flex space-x-2 space-y-0">
                     <FormControl>
                       <Checkbox
-                        checked={field.value}
-                        onCheckedChange={field.onChange}
+                        id="terms"
+                        checked={!!field.value}
+                        onCheckedChange={(checked) =>
+                          field.onChange(checked === true)
+                        }
                       />
                     </FormControl>
                     <div className="leading-none">
